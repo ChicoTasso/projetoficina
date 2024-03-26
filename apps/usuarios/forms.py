a4bb1965caa3fcdc88f3fec26febedd0315250dc
ChicoTasso <tassomaisdogi@gmail.com>
2024-03-25 21:33:56 -0300

    def user login

---
+++ apps/usuarios/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+
+class LoginForm(forms.Form):
+
+    usuario = forms.CharField(label='Nome de Usuário', required=True)
+    senha = forms.CharField(label='Senha', required=True, widget=forms.PasswordInput())
+
+        
